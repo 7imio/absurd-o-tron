ecda5856ff145d76728ec02bf6be72b204d8942c
Anthony deplanque <anthony.deplanque3184@gmail.com>
2025-04-24 17:23:14 +0200

    title + favicon

--- src/Helpers/generate-random-name.ts
+++ src/Helpers/generate-random-name.ts
@@ -1,14 +1,15 @@
-import { getRandom } from './get-random';
-import { middles, prefixes, suffixes, titles } from './name-lists';
+import { getRandom } from "./get-random";
+import { middles, prefixes, suffixes, titles } from "./name-lists";
 
 export const generateRandomName = (): string => {
-    const addTitle = Math.random() < 0.3; // 50% chance to add a title
-    const addMiddle = Math.random() < 0.6; // 60% chance to add a middle
+  const addTitle = Math.random() < 0.3; // 30% chance to add a title
+  const addMiddle = Math.random() < 0.6; // 60% chance to add a middle
 
-    const randomPrefix = getRandom(prefixes);
-    const randomMiddle = getRandom(middles);
-    const randomSuffix = getRandom(suffixes);
-    
-    return `${addTitle ? getRandom(titles) : ''} ${randomPrefix}${addMiddle? randomMiddle:""}${randomSuffix}`
+  const randomPrefix = getRandom(prefixes);
+  const randomMiddle = getRandom(middles);
+  const randomSuffix = getRandom(suffixes);
 
-}
+  return `${addTitle ? getRandom(titles) : ""} ${randomPrefix}${
+    addMiddle ? randomMiddle : ""
+  }${randomSuffix}`;
+};
