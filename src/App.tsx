144ee792081733c3d1220473b005c85d16238e2d
anthony deplanque <anthony.deplanque@csgroup.eu>
2025-04-24 16:47:44 +0200

    improved css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Footer from './Components/Footer'
 import GenerateName from './Components/GenerateName'
 import NameDisplay from './Components/NameDisplay'
 import './index.css'
@@ -7,12 +8,14 @@ function App() {
 
   const [name, setName] = useState<string>('')
 
-  return (
-    <>
+  return (<>
+    <div className="app">
     <h1>Absurd-o-tron</h1>
     <GenerateName setName={setName} />
     <NameDisplay name={name} />
-    </>
+    </div>
+    <Footer />
+  </>
   )
 }
 
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer:React.FC = () => {
+    return (
+        <footer>
+            <p>Made with ❤️ by <a href="https://www.twitch.tv/seteemio">Seteemio</a></p>
+            <p>Source code available on <a href="https://github.com/7imio/absurd-o-tron">GitHub</a></p>
+        </footer>
+    )  
+
+}
+export default Footer;
